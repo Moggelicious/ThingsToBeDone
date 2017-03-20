d467ac526a698f079a57fc4973cf84a5d4d3bbd7
Morgan <Moggelicious@hotmail.com>
2017-03-20 18:37:15 +0100

    recyclerview todo 0.3

--- app/src/main/java/com/example/morga/thingstobedone/MainActivity.java
+++ app/src/main/java/com/example/morga/thingstobedone/MainActivity.java
@@ -10,38 +10,11 @@ import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
-private RecyclerView recView;
-    private TTBDAdapter adapter;
-
-    @Override
-    public TTBDAdapter.TTBDHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.list_item, parent, false);
-        return new TTBDHolder(view);
-    }
- 
-    @Override
-    public void onBindViewHolder(DerpHolder holder, int position) {
-        ListItem item = listData.get(position);
-        holder.title.setText(item.getTitle());
-        holder.icon.setImageResource(item.getImageResId());
-    }
- 
-    @Override
-    public int getItemCount() {
-        return listData.size();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recView = (RecyclerView)findViewById(R.id.rec_list);
-        recView.setLayoutManager(new LinearLayoutManager(this));
-
-        adapter = new TTBDAdapter(TTBDData.getListData(),this);
-        recView.setAdapter(adapter);
     }
-
-
 }

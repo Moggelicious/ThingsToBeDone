2c9616b931bf86e80d6d259207c0060adb1533aa
Morgan <Moggelicious@hotmail.com>
2017-03-20 20:03:30 +0100

    recyclerview 0.12 learning stuff

--- app/src/main/java/com/example/morga/thingstobedone/ListItem.java
+++ app/src/main/java/com/example/morga/thingstobedone/ListItem.java
@@ -5,4 +5,23 @@ package com.example.morga.thingstobedone;
  */
 
 public class ListItem {
+
+    private String title;
+    private int imageResId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public void setImageResId(int imageResId) {
+        this.imageResId = imageResId;
+    }
 }

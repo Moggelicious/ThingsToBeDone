d3e257ceb7a8f0c6170dfa47f2ec9419518f6739
Morgan <Moggelicious@hotmail.com>
2017-03-31 20:37:33 +0200

    recyclerview 0.15 learning stuff

--- app/src/main/java/com/example/morga/thingstobedone/MainActivity.java
+++ app/src/main/java/com/example/morga/thingstobedone/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.morga.thingstobedone;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ToDoAdapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_list);
+        //LayoutManager: Grid, Staggered
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new ToDoAdapter(ToDoData.getListData(), this);
+        recyclerView.setAdapter(adapter);
 
     }
 }
--- app/src/main/java/com/example/morga/thingstobedone/ToDoAdapter.java
+++ app/src/main/java/com/example/morga/thingstobedone/ToDoAdapter.java
@@ -1,8 +1,61 @@
 package com.example.morga.thingstobedone;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by Morga on 2017-03-20.
  */
 
-public class ToDoAdapter {
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ToDoHolder> {
+
+    private List<ListItem> listData;
+    private LayoutInflater inflater;
+
+    public ToDoAdapter (List<ListItem> listdata, Context c){
+        this.inflater = LayoutInflater.from(c);
+        this.listData = listData;
+    }
+
+
+    @Override
+    public ToDoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.list_item, parent, false);
+        return new ToDoHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ToDoHolder holder, int position) {
+        ListItem item = listData.get(position);
+        holder.title.setText(item.getTitle());
+        holder.icon.setImageResource(item.getImageResId());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    class ToDoHolder extends RecyclerView.ViewHolder {
+
+        private TextView title;
+        private ImageView icon;
+        private View container;
+
+        public ToDoHolder(View itemView) {
+            super(itemView);
+
+            title = (TextView)itemView.findViewById(R.id.label_item_text);
+            icon = (ImageView)itemView.findViewById(R.id.image_item_icon);
+            container = itemView.findViewById(R.id.container_item_root);
+        }
+    }
 }
